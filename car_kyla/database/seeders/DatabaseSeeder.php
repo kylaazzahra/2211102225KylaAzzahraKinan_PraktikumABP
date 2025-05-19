66483ec302e125fbf81c95f334d02bf4cd645098
Kyla <kylaazzahrak22@Gmail.com>
2025-05-19 14:40:50 +0700

    ABP

---
+++ car_kyla/database/seeders/DatabaseSeeder.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace Database\Seeders;
+use Illuminate\Support\Facades\Hash;
+use App\Models\User;
+// use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+
+class DatabaseSeeder extends Seeder
+{
+    /**
+     * Seed the application's database.
+     */
+    public function run(): void
+{
+    User::create([
+        'name' => 'Admin User',
+        'email' => 'admin@example.com',
+        'password' => Hash::make('password123'),
+    ]);
+}
+}
